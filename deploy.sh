a476be8ff2c02566daaebdf4b1697293d7d2f657
gfulton-redhat <gfulton@redhat.com>
2022-01-21 15:47:46 -0800

    add other versions

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-kustomize build . | oc delete -f-
-sleep 5
+
 kustomize build . | oc apply -f-
 sleep 5
 oc patch deployment/productpage-v1 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
-oc patch deployment/ratings-v1 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
 oc patch deployment/details-v1 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+oc patch deployment/details-v2 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
 oc patch deployment/reviews-v1 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
 oc patch deployment/reviews-v2 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
 oc patch deployment/reviews-v3 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+oc patch deployment/ratings-v1 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+oc patch deployment/ratings-v2 -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+oc patch deployment/ratings-v2-mysql -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+oc patch deployment/ratings-v2-mysql-vm -n bookinfo -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
