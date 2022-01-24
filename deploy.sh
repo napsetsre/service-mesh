764890a235afad970b6af642738e8dd3196426b8
gfulton-redhat <gfulton@redhat.com>
2022-01-24 12:12:44 -0800

    add certs

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ DEPLOYMENTS=( productpage-v1 details-v1 details-v2 reviews-v2 reviews-v3 ratings
 NAMESPACE="bookinfo"
 for deployment in "${DEPLOYMENTS[@]}"
 do
-   oc patch deployment/${deployment} -n $NAMESPACE -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+   oc patch deployment/${deployment} \
+      -n $NAMESPACE -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
 done
+
