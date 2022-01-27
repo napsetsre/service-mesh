16007e43d6deab07d14f69c822f99a4d00e5da7e
gfulton-redhat <gfulton@redhat.com>
2022-01-27 14:02:21 -0800

    add deployment configs

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+
 # Apply changes
 kustomize build . | oc apply -f-
-
+sleep 5
 # Redeploy
-DEPLOYMENTS=( productpage-v1 details-v1 details-v2 reviews-v2 reviews-v3 ratings-v1 ratings-v2 ratings-v2-mysql ratings-v2-mysql-vm mongodb-v1 )
-NAMESPACE="bookinfo"
-for deployment in "${DEPLOYMENTS[@]}"
-do
-   oc patch deployment/${deployment} \
-      -n $NAMESPACE -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
-done
+#DEPLOYMENTS=( productpage-v1 details-v1 details-v2 reviews-v2 reviews-v3 ratings-v1 ratings-v2 ratings-v2-mysql ratings-v2-mysql-vm mongodb-v1 )
+#NAMESPACE="bookinfo"
+#for deployment in "${DEPLOYMENTS[@]}"
+#do
+#   oc patch deploymentconfig/${deployment} \
+#      -n $NAMESPACE -p '{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt": "'`date +%s`'"}}}}}'
+#done
 
